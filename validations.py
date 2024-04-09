4e553bce9e8fe6c180422f30fd5079b2620f38db
Karawan Sh.Y <101515668+karawanshy@users.noreply.github.com>
2024-04-10 02:47:08 +0300

    Update validations.py

--- validations.py
+++ validations.py
@@ -1,13 +1,19 @@
 import re
 
+# check if the ip address is in a legal format
 def validate_ip(addr):
     pattern = r'^([0-9]{1,3}\.){3}[0-9]{1,3}$'
+
+    # if matches the pattern
     if re.match(pattern, addr):
+        
+        # check if each octat is between 0 and 255
         parts = addr.split(".")
         for part in parts:
             if int(part) < 0 or int(part) > 255:
                 return False
         return True
+        
     return False
 
 def validate_cidr(cidr):
